a991c0c6e45f7b2a8bdf09ab9993c609c130a49f
Ruben <22rpf2006@gmail.com>
2024-09-23 13:15:50 +0200

    Añadir edad y año de nacimiento al programa

---
+++ prueba1.java
@@ -0,0 +1,6 @@
+public class prueba1 {
+    public static void main(String[] args) {
+        System.out.println("Perez Ferrero");
+	System.out.println("Tengo 18 años, naci el 22/11/2006");
+    }
+}
