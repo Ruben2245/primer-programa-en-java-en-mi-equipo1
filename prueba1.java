46677b2a6b0d933cb66bba6bbd1d56ad48f0f764
Ruben <22rpf2006@gmail.com>
2024-09-25 13:12:06 +0200

    Subiendo repositorio local a GitHub

--- prueba1.java
+++ prueba1.java
@@ -1,4 +1,4 @@
-public class prueba1 {
+public class prueba2 {
     public static void main(String[] args) {
         System.out.println("Perez Ferrero");
 	System.out.println("Tengo 18 años, naci el 22/11/2006");
