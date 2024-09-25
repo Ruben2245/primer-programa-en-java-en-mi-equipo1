d83d3e0b67fbf1d27c2016f8a3d23293cbac6d22
Ruben <22rpf2006@gmail.com>
2024-09-25 14:24:23 +0200

    Eliminado linea apellidos

--- prueba2.java
+++ prueba2.java
@@ -1,6 +1,5 @@
 public class prueba2 {
     public static void main(String[] args) {
-        System.out.println("Perez Ferrero");
 	System.out.println("Tengo 18 años, naci el 22/11/2006");
 	System.out.println("Mi comida favorita es xxxx");
     }
